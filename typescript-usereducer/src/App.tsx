c54334bf55e5ed038d0a45272720cc1e6965c515
Seema Ullal <seema.ullal@gusto.com>
2021-01-28 19:37:05 -0800

    add types

--- typescript-usereducer/src/App.tsx
+++ typescript-usereducer/src/App.tsx
@@ -1,32 +1,57 @@
 import * as React from "react";
 
-/**
- * The App component uses a custom useFetch hook which
- * tracks its state with useReducer.
- *
- * You'll need to implement the fetchReducer function and
- * assign types to its parameters.
- *
- * Don't forget: Reducers are much easier to implement
- * in TypeScript if you use a discriminating union for the
- * Action type.
- *
- * Add any other type annotations where necessary, By the time
- * you are done, the red squiggles should be gone.
- *
- * Bonus points: Try making useFetch into a generic function so
- * it can be used with more than just the dad joke API.
- */
-
 interface DadJokeResponse {
   id: string;
   joke: string;
   status: 200;
 }
+
+type useFetchDispatchAction = "loading" | "data" | "error";
+type UseFetchState = {
+  state: useFetchDispatchAction;
+  data: DadJokeResponse | null;
+  error: { message: string } | null;
+};
 const JOKE_URL = "https://icanhazdadjoke.com/";
 
-function fetchReducer(state: unknown, action: unknown) {
-  //  Implement your reducer here.
+function fetchReducer(
+  state: UseFetchState,
+  {
+    data,
+    error,
+    type,
+  }: {
+    data?: DadJokeResponse;
+    error?: { message: string };
+    type: useFetchDispatchAction;
+  }
+): UseFetchState {
+  switch (type) {
+    case "loading":
+      return {
+        state: "loading",
+        data: null,
+        error: null,
+      };
+    case "error":
+      if (error) {
+        return {
+          state: "error",
+          data: null,
+          error: error,
+        };
+      }
+      break;
+    case "data":
+      if (data) {
+        return {
+          state: "data",
+          data: data,
+          error: null,
+        };
+      }
+      break;
+  }
   return state;
 }
 
@@ -34,7 +59,7 @@ function useFetch(url: string) {
   const [state, dispatch] = React.useReducer(fetchReducer, {
     state: "loading",
     data: null,
-    error: null
+    error: null,
   });
 
   React.useEffect(() => {
@@ -42,8 +67,8 @@ function useFetch(url: string) {
       try {
         const response = await fetch(url, {
           headers: {
-            accept: "application/json"
-          }
+            accept: "application/json",
+          },
         });
         const data: DadJokeResponse = await response.json();
         dispatch({ type: "data", data });
